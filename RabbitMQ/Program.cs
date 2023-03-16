aec7e519c314a539dcfd3e2c0c821998b889f897
Anastas Kostov <Anastas.Kostov@tbs.tech>
2023-03-16 15:41:50 +0200

    initial project setup

---
+++ RabbitMQ/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using RabbitMQ;
+
+IServiceCollection services = new ServiceCollection();
+IServiceProvider serviceProvider = BuildServiceProvider(services);
+
+
+
+
+static IServiceProvider BuildServiceProvider(IServiceCollection services)
+{
+    var configuration = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName)
+        .AddJsonFile("appsettings.json", optional: false)
+        .Build();
+
+    services.AddRabbitMq(configuration);
+
+    return services.BuildServiceProvider();
+}
---
+++ RabbitMQ/RabbitMqConfiguration.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using RabbitMq;
+using RabbitMQ.Client;
+
+namespace RabbitMQ;
+
+internal static class RabbitMqConfiguration
+{
+    private const string SECTION_NAME = "rabbitmq";
+
+    internal static IServiceCollection AddRabbitMq(this IServiceCollection services, IConfiguration configuration)
+    {
+        var rabbitMqOptions = new RabbitMqOptions();
+        configuration.GetSection(SECTION_NAME).Bind(rabbitMqOptions);
+
+        if (rabbitMqOptions.HostNames is null || !rabbitMqOptions.HostNames.Any())
+        {
+            throw new ArgumentException("RabbitMQ hostnames are not specified.", nameof(rabbitMqOptions.HostNames));
+        }
+
+        var connectionFactory = new ConnectionFactory
+        {
+            Port = rabbitMqOptions.Port,
+            VirtualHost = rabbitMqOptions.VirtualHost,
+            UserName = rabbitMqOptions.Username,
+            Password = rabbitMqOptions.Password,
+            DispatchConsumersAsync = true,
+        };
+
+        services.AddSingleton(connectionFactory.CreateConnection(rabbitMqOptions.HostNames.ToList()));
+        var connection = connectionFactory.CreateConnection();
+
+        services.AddSingleton<IConnection>(connection);
+        services.AddSingleton<IModel>(connection.CreateModel());
+
+        return services;
+    }
+}
